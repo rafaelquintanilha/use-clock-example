986c83d2676fd6547968a67dc85736ecfe2a2836
Rafael Quintanilha <rafaelquintanilha@poli.ufrj.br>
2019-02-21 23:48:05 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,83 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Prism from 'prismjs';
+import './prism.css';
+import useClock from 'use-clock';
 
-class App extends Component {
+const snippet = `import React from 'react';
+import useClock from 'use-clock';
+
+function HookExample() {
+  // Initialize by passing any string accepted by
+  // https://momentjs.com/docs/#/displaying/format/
+  // and an optional period (defaults to 1000ms)
+  // onTimezone accepts a string with a valid timezone as per
+  // https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  // raw is a moment object
+  const { time, onTimezone, raw } = useClock("HH:mm:ss");
+  const londonTime = onTimezone("Europe/London");
+  const brasiliaTime = onTimezone("America/Sao_Paulo");
+  return (
+    <div>
+      <div><b>Computer time:</b> {time}</div>
+      <div><b>London time:</b> {londonTime}</div>
+      <div><b>Brasilia time:</b> {brasiliaTime}</div>
+      <div><b>Raw time:</b> {raw.format("dddd, MMMM Do YYYY, h:mm:ss a")}</div>
+    </div>
+  );
+}
+`;
+
+class App extends React.Component {
+
+  componentDidMount() {
+    Prism.highlightAll();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <>
+        <header>
+          <h1>useClock()</h1>
         </header>
-      </div>
+        <main>
+          <p>
+            $ npm install --save use-clock
+          </p>
+          <HookExample />
+          <hr />
+          <pre>
+            <code className="language-javascript">          
+              {snippet}   
+            </code>
+          </pre>
+          <hr />
+          <footer>
+            <a href="https://github.com/rafaelquintanilha/use-clock">GitHub</a>
+            &nbsp;
+            •
+            &nbsp;
+            Created by
+            &nbsp;
+            <a href="https://rafaelquintanilha.com">Rafael Quintanilha</a>
+          </footer>
+        </main>
+      </>
     );
   }
+}
+
+function HookExample() {
+  const { time, onTimezone, raw } = useClock("HH:mm:ss");
+  const londonTime = onTimezone("Europe/London");
+  const brasiliaTime = onTimezone("America/Sao_Paulo");
+  return (
+    <div>
+      <div><b>Computer time:</b> {time}</div>
+      <div><b>London time:</b> {londonTime}</div>
+      <div><b>Brasilia time:</b> {brasiliaTime}</div>
+      <div><b>Raw time:</b> {raw.format("dddd, MMMM Do YYYY, h:mm:ss a")}</div>
+    </div>
+  );
 }
 
 export default App;
